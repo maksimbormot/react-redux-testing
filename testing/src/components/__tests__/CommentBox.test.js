43f670efa28b702eadf0d7aff28af5fa0ee41a14
Rus Hughes <russell.hughes@gmail.com>
2018-05-26 21:12:34 +0900

    now we can do code coverage!

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -8,7 +8,39 @@ beforeEach(() => {
   wrapped = mount(<CommentBox />);
 });
 
+afterEach(() => {
+  wrapped.unmount();
+})
+
 it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a text area that users can type in', () => {
+
+  const comment = 'this is a new comment'
+
+  wrapped.find('textarea').simulate('change', {
+    target: { value: comment }
+  });
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual(comment);
+});
+
+it('when form is submitted, text area gets emptied', () => {
+
+  const comment = 'this is a new comment'
+
+  wrapped.find('textarea').simulate('change', {
+    target: { value: comment }
+  });
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual(comment);
+
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+
 });
